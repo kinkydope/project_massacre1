7734233fa8f7f2b44375eda769effe051e4b990c
kinkydope <mr.grinas@gmail.com>
2024-07-14 12:33:59 +0300

    first_commit
    Animated and textured models
    User Input

---
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        Transform cameraObject;
+        InputHandler inputHandler;
+        Vector3 moveDirection;
+
+        [HideInInspector]
+        public Transform myTransform;
+
+        void Start()
+        {
+
+        }
+
+
+    }
+}
